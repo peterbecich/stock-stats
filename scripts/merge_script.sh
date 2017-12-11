37d767485aaa9fbef16c8de9849b4cd1bab35e8d
Peter Becich <peterbecich@gmail.com>
2017-12-10 19:11:34 -0800

    travis pushes to production; new keys

---
+++ scripts/merge_script.sh
@@ -0,0 +1,17 @@
+#https://reformatcode.com/code/git/automatic-merge-branch-into-master-on-sucessful-build-in-travis
+
+echo "merge_script.sh"
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "merge into production"
+
+    git checkout production || exit
+    git merge "$TRAVIS_COMMIT" || exit
+    git push origin production || exit
+fi
+
+if [ "$TRAVIS_BRANCH" == "production" ]; then
+    echo "CI build of production branch successful"
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    echo "logged into Docker Hub"
+    docker push peterbecich/stock-stats
+fi
